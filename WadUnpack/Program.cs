90098ece650ce6a9ca3e302f81db256ca7492f7c
chrissx <christian.chrissicx2004@freenet.de>
2018-09-08 20:54:37 +0200

    Simplified code, improved size and entry count formatting.

--- WadUnpack/Program.cs
+++ WadUnpack/Program.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using static System.Console;
+using static System.IO.Path;
+using static System.IO.Directory;
+using static System.IO.File;
+using static System.Environment;
+using static System.Drawing.Color;
 
 namespace WadUnpack
 {
@@ -14,9 +17,9 @@ namespace WadUnpack
             SetWindowSize(70, 30);
 
             string wadfile = args[0];
-            string basedir = "unpacked_" + Path.GetFileNameWithoutExtension(wadfile);
+            string basedir = "unpacked_" + GetFileNameWithoutExtension(wadfile);
 		    reader r = new reader(wadfile);
-            Directory.CreateDirectory(basedir);
+            CreateDirectory(basedir);
 
             WriteLine("File Size:         " + r.format_size());
             WriteLine("Directory Entries: " + r.format_entry_count());
@@ -24,14 +27,14 @@ namespace WadUnpack
 
 		    List<entry> dir = new List<entry>();
 
-            for (int i = 0; i < r.entries; i++)
+            for (int j = 0; j < r.entries; j++)
                 dir.Add(new entry(r));
 
             int i = 7;
 
-		    WriteLine("-----------------------------");
-		    WriteLine($@"Unpacking into {basedir}\...");
-            WriteLine("-----------------------------");
+		    WriteLine("---------------------------------");
+		    WriteLine($"Unpacking into {basedir}\\...");
+            WriteLine("---------------------------------");
             foreach (entry e in dir)
             {
                 if(i > 28)
@@ -41,9 +44,9 @@ namespace WadUnpack
                 }
                 int j = e.type;
                 if (j != 67)
-                    Console.WriteLine($"Cannot unpack an entry of type {j}.");
+                    WriteLine($"Cannot unpack an entry of type {j}.");
                 else
-                    unpack_texture(r, e, Path.Combine(basedir, e.name + ".png"));
+                    unpack_texture(r, e, Combine(basedir, e.name + ".png"));
                 i++;
 		    }
 		    WriteLine("...Done.");
@@ -54,32 +57,32 @@ namespace WadUnpack
 
 	    static void unpack_texture(reader r, entry d, string dest)
         {
-            r.index = d.offset;
-            r.read_string(16);
+            r.seek(d.offset);
+            r.reads(16);
 		    int w = r.read32();
 		    int h = r.read32();
             int pixels = w * h;
             int offset1 = r.read32();
-            r.index += 8;
+            r.skip(8);
             int offset4 = r.read32();
 
-		    byte[] texture = new byte[pixels];
-            r.index = d.offset + offset1;
-		    for(int i = 0; i < pixels; i++)
+		    int[] texture = new int[pixels];
+            r.seek(d.offset + offset1);
+		    for (int i = 0; i < pixels; i++)
                 texture[i] = r.read8();
 
-            r.index = d.offset + offset4 + pixels / 64 + 2;
+            r.seek(d.offset + offset4 + pixels / 64 + 2);
 		    Color[] clut = new Color[256];
 		    for(int i = 0; i < 256; i++)
                 clut[i] = r.readc();
 
 		    Bitmap img = new Bitmap(w, h);
 
-		    for(int x = 0; x < w; x++)
-			    for(int y = 0; y < h; y++)
+		    for (int x = 0; x < w; x++)
+			    for (int y = 0; y < h; y++)
 				    img.SetPixel(x, y, clut[texture[x + y * w]]);
 
-		    WriteLine(dest + "         ");
+		    WriteLine(dest + "\t");
             img.Save(dest, ImageFormat.Png);
 	    }
     }
@@ -89,25 +92,26 @@ namespace WadUnpack
         public int offset;
         public int size;
         public int type;
+        public int compression;
         public string name;
 
         public entry(reader r)
         {
             offset = r.read32();
             size = r.read32();
-            r.index += 4;
+            r.skip(4);
             type = r.read8();
+            compression = r.read8();
             
-            if (r.read8() != 0)
+            if (compression != 0)
             {
-                WriteLine("Compressed textures are not supported.");
+                WriteLine($"Compressed textures (compression: {compression}) are not supported.");
                 Read();
-                Environment.Exit(1);
+                Exit(1);
             }
 
-            r.index++;
-            r.index++;
-            name = r.read_string(16).Replace("\0", "");
+            r.skip(2);
+            name = r.reads(16).Replace("\0", "");
         }
 
         public override string ToString() => $"{offset}\t{name} ({size} bytes)";
@@ -115,24 +119,23 @@ namespace WadUnpack
 
     class reader
     {
-        public int index = 0;
+        int index = 0;
         byte[] data;
         public int entries;
         public int offset;
 
         public reader(string file)
         {
-            data = File.ReadAllBytes(file);
+            data = ReadAllBytes(file);
 
-            string m = read_string(4);
+            string m = reads(4);
 
             if (m == "WAD3" || m == "WAD2")
                 WriteLine(m + " format");
             else
             {
                 WriteLine("Not a WAD file.");
-                Read();
-                Environment.Exit(1);
+                Exit(1);
             }
 
             entries = read32();
@@ -141,36 +144,47 @@ namespace WadUnpack
 
         public string format_size()
         {
-            long l = data.Length;
-            return (l < 1000 ? l + "B" : l < 1000000 ? (l / 1000F).ToString("F") + "kB" : (l / 1000000F).ToString("F") + "MB");
+            int l = data.Length;
+            return l < 1000 ? l + "B" : l < 1000000 ? (l / 1000D).ToString("F2") + "kB" : (l / 1000000D).ToString("F2") + "MB";
         }
 
         public string format_entry_count()
         {
-            return (entries < 1000 ? entries + "E" : (float)entries / 1000 + "kE");
+            return entries < 1000 ? entries + "E" : (entries / 1000D).ToString("F2") + "kE";
         }
 
-        public string read_string(int len)
+        //not good for long strings because it doesnt use stringbuilder
+        public string reads(int len)
         {
             string s = "";
             for (int i = 0; i < len; i++)
-                s += (char)data[index++];
+                s += (char)read8();
             return s;
         }
 
-        public byte read8()
+        public int read8()
         {
-            return data[index++];
+            return index < data.LongLength ? read8() : -1;
         }
 
         public int read32()
         {
-            return data[index++] | (data[index++] << 8) | (data[index++] << 16) | (data[index++] << 24);
+            return read8() | (read8() << 8) | (read8() << 16) | (read8() << 24);
         }
 
         public Color readc()
         {
-            return Color.FromArgb(data[index++], data[index++], data[index++]);
+            return FromArgb(data[index++], data[index++], data[index++]);
+        }
+
+        public void seek(int idx)
+        {
+            index = idx;
+        }
+
+        public void skip(int count)
+        {
+            index += count;
         }
     }
 }
